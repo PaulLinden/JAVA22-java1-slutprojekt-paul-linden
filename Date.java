fc7149ae55b2dbd3b68d1b52d59a6ff788779bd8
PaulLinden <112555968+PaulLinden@users.noreply.github.com>
2022-11-16 13:41:06 +0100

    Update Date.java

--- Date.java
+++ Date.java
@@ -4,14 +4,14 @@ import org.joda.time.LocalDate;
 
 public class Date  {
 	
-	public static LocalDate day = new LocalDate();
-	public static LocalDate monday = day.withDayOfWeek(org.joda.time.DateTimeConstants.MONDAY);
-	public static LocalDate tuesday = day.withDayOfWeek(org.joda.time.DateTimeConstants.TUESDAY);
-	public static LocalDate wednesday = day.withDayOfWeek(org.joda.time.DateTimeConstants.WEDNESDAY);
-	public static LocalDate thursday = day.withDayOfWeek(org.joda.time.DateTimeConstants.THURSDAY);
-	public static LocalDate friday = day.withDayOfWeek(org.joda.time.DateTimeConstants.FRIDAY);
-	public static LocalDate saturday = day.withDayOfWeek(org.joda.time.DateTimeConstants.SATURDAY);
-	public static LocalDate sunday = day.withDayOfWeek(org.joda.time.DateTimeConstants.SUNDAY);
+	public static LocalDate todaysDate = new LocalDate();
+	public static LocalDate mondayDate = todaysDate.withDayOfWeek(org.joda.time.DateTimeConstants.MONDAY);
+	public static LocalDate tuesdayDate = todaysDate.withDayOfWeek(org.joda.time.DateTimeConstants.TUESDAY);
+	public static LocalDate wednesdayDate = todaysDate.withDayOfWeek(org.joda.time.DateTimeConstants.WEDNESDAY);
+	public static LocalDate thursdayDate = todaysDate.withDayOfWeek(org.joda.time.DateTimeConstants.THURSDAY);
+	public static LocalDate fridayDate = todaysDate.withDayOfWeek(org.joda.time.DateTimeConstants.FRIDAY);
+	public static LocalDate saturdayDate = todaysDate.withDayOfWeek(org.joda.time.DateTimeConstants.SATURDAY);
+	public static LocalDate sundayDate = todaysDate.withDayOfWeek(org.joda.time.DateTimeConstants.SUNDAY);
 	
 }	
 	
